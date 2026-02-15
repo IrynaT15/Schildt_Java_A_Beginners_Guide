ef15e27b10941735dd9451e148fd050b86b25fc6
Iryna <irina.tainikova@gmail.com>
2026-02-15 16:25:05 +0100

    add GalToLitTable exercise

---
+++ src/chapter1/GalToLitTable.java
@@ -0,0 +1,20 @@
+package chapter1;
+
+public class GalToLitTable {
+    public static void main(String[] args) {
+        double gallons;
+        double liters;
+        int counter = 0;
+
+        for (gallons = 1; gallons <= 100; gallons ++) {
+            liters = gallons * 3.7854;
+            System.out.printf("%s gallons is %s liters", gallons, liters);
+            System.out.println();
+            counter ++;
+            if (counter == 10) {
+                System.out.println("");
+                counter = 0;
+            }
+        }
+    }
+}
