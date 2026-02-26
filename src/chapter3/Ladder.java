d065d4ef708f2dcd155e9ea9d7e1dc1fb7358555
Iryna <irina.tainikova@gmail.com>
2026-02-26 22:42:27 +0100

    add Ladder class

---
+++ src/chapter3/Ladder.java
@@ -0,0 +1,21 @@
+package chapter3;
+
+public class Ladder {
+    public static void main(String[] args) {
+        int x;
+
+        for (x = 0; x < 6; x++) {
+            if (x == 1) {
+                System.out.println("x is 1");
+            } else if (x == 2) {
+                System.out.println("x is 2");
+            } else if (x == 3) {
+                System.out.println("x is 3");
+            } else if (x == 4) {
+                System.out.println("x is 4");
+            } else {
+                System.out.println("x is out of the range of 1-4");
+            }
+        }
+    }
+}
