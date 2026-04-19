434f6c7caba806a8e14b3444a0f933ed41043c2f
Iryna <irina.tainikova@gmail.com>
2026-04-19 14:38:24 +0200

    add Empty, Empty2, Empty3 class

---
+++ src/chapter3/Empty.java
@@ -0,0 +1,12 @@
+package chapter3;
+
+public class Empty {
+    public static void main(String[] args) {
+        int i;
+
+        for (i = 0; i < 10;) {
+            System.out.println("Cycle #" + i);
+            i++;
+        }
+    }
+}
---
+++ src/chapter3/Empty2.java
@@ -0,0 +1,13 @@
+package chapter3;
+
+public class Empty2 {
+    public static void main(String[] args) {
+        int i;
+
+        i = 0;
+        for (;i < 10;) {
+            System.out.println("Cycle #" + i);
+            i++;
+        }
+    }
+}
