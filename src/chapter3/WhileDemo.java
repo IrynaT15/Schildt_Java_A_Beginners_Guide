cb21c97712f45f075d558c037c5c8e5d96806b44
Iryna <irina.tainikova@gmail.com>
2026-04-19 14:51:25 +0200

    add WhileDemo class

---
+++ src/chapter3/WhileDemo.java
@@ -0,0 +1,13 @@
+package chapter3;
+
+public class WhileDemo {
+    public static void main(String[] args) {
+        char ch;
+
+        ch = 'a';
+        while (ch <= 'z') {
+            System.out.println(ch);
+            ch++;
+        }
+    }
+}
